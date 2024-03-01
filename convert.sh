ecdc0006159a031bc19d8474576f6d053212ded3
David Eidelman <eidelman.david@gmail.com>
2024-02-29 20:54:50 -0500

    finalize convert.sh

---
+++ convert.sh
@@ -0,0 +1,87 @@
+#!/usr/env/bin bash
+
+#
+# convert.sh
+#
+# script to coordinate the conversion of .rds files to .h5ad
+#
+# Copyright (c) 2024 David Eidelman. MIT License.
+#
+
+OVERWRITE=false
+
+Help(){
+    # echo "A script to convert Seurat .rds files into AnnData .h5ad files."
+    echo "Usage: convert [-o|-h] filename"
+    echo    filename needs to have the suffix .rds
+    echo
+    echo "options:"
+    echo "o     Overwrite an existing .h5ad file."
+    echo "h     Print this help."
+    echo "V     Print software version and license information."
+    echo
+}
+
+
+#
+# ensure filename is in the form *.rds
+#
+valid_filename(){
+    [[ $1 =~ ^.*\.rds$ ]] 
+}
+
+# echo "Welcome to the rdsad file conversion program."
+# echo
+
+while getopts ":ho:" option; do
+    case $option in
+    h) # display help
+        Help
+        exit;;
+    o) # allow overwrite
+        echo "Overwriting output"
+        OVERWRITE=true
+        ;;
+    \?) # invalid option
+        echo "Invalid option"
+        echo
+        Help
+        exit;;
+    esac
+done
+
+FILENAME=${@: -1}
+if [ -z $FILENAME ]; then
+    # echo -n "Enter a filename:"
+    # read -r FILENAME
+    Help;
+    exit 1
+fi
+if ! valid_filename $FILENAME; then
+    echo Filename $FILENAME  is not valid.
+    echo The Filename must end in .rds
+    exit 1
+fi
+if ! [[ -e $FILENAME ]]; then
+    echo Unable to find $FILENAME.  Did you enter the filename correctly?
+    exit 1
+fi
+RSCRIPT="rdsad-convert.R"
+if ! [[ -e $RSCRIPT ]]; then
+    echo "Unable to find R script file $RSCRIPT. Did you install the rdsad system correctly?"
+    exit 1
+fi
+if ! [[ -e ${FILENAME/.rds/.h5ad} ]] || $OVERWRITE; then
+    echo "Converting the rds file."
+    Rscript rdsad-convert.R "$FILENAME"
+else 
+    echo "Conversion already completed."
+fi
+INTERFILE=${FILENAME/".rds"/".h5seurat"}
+if [[ -e $INTERFILE ]]; then
+    echo "Removing intermediate file: $INTERFILE"
+    rm $INTERFILE
+fi
+
+echo
+echo rdsad has completed its tasks.
--- rdsad-convert.R
+++ rdsad-convert.R
@@ -6,7 +6,7 @@
 # this script converts .rds files into .h5ad files using the approach outlined
 # by Z.Q. Fang (https://zqfang.github.io/2020-04-28-seurat2scanpy).
 # However, instead of using the method based on MuDataSeurat, which doesn't seem to work
-# as of 2024-02-29, it uses the SeuratDisk method recommended by tehe SatijaLab, who
+# as of 2024-02-29, it uses the SeuratDisk method recommended by the SatijaLab, who
 # are the authors of the Seurat system.
 #
 # Copyright (c) 2024 by David Eidelman. All rights reserved.  MIT License.
